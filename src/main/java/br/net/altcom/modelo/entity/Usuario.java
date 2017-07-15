df4b308b788d9b78dffbfe3171b77868f068cde1
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-15 00:19:09 -0300

    Adicionando entity Usuario

---
+++ src/main/java/br/net/altcom/modelo/entity/Usuario.java
@@ -0,0 +1,41 @@
+package br.net.altcom.modelo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer codigo;
+	private String email;
+	private String senha;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
