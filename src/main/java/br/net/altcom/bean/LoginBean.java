e7f6f99062ab20e669d70b08855a05b813fb20f9
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-15 02:35:31 -0300

    Injetando usuarioDAO na classe loginBean

--- src/main/java/br/net/altcom/bean/LoginBean.java
+++ src/main/java/br/net/altcom/bean/LoginBean.java
@@ -1,13 +1,18 @@
 package br.net.altcom.bean;
 
 import javax.enterprise.inject.Model;
+import javax.inject.Inject;
 
+import br.net.altcom.dao.UsuarioDAO;
 import br.net.altcom.modelo.entity.Usuario;
 
 @Model
 public class LoginBean {
 
 	private Usuario usuario = new Usuario();
+	
+	@Inject
+	private UsuarioDAO usuarioDAO;
 	
 	public String login(){
 		return "";
