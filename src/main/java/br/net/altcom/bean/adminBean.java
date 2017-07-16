38b3b357c9e88804a31f8c1597fa0dad109d1034
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 13:46:09 -0300

    Utilizando a classe excel no loginBean para abrir arquivos upados e adicionando em uma lista todas as planilhas que sao mostrada em uma lista na pagina admin.xhtml

--- src/main/java/br/net/altcom/bean/adminBean.java
+++ src/main/java/br/net/altcom/bean/adminBean.java
@@ -1,25 +1,51 @@
 package br.net.altcom.bean;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.inject.Model;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.primefaces.model.UploadedFile;
+
+import br.net.altcom.excel.Excel;
 
 @Model
 public class adminBean {
 
 	private UploadedFile file;
-	 
-    public void upload() {
-        if(file != null) {
-        	System.out.println("Arquivo: " + file.getFileName() + " Upload");
-        }
-    }
-    
-    public UploadedFile getFile() {
-    	return file;
-    }
-    
-    public void setFile(UploadedFile file) {
-    	this.file = file;
-    }
+	private List<Sheet> sheets = new ArrayList<>();
+
+	public void upload() {
+		if (file != null) {
+			System.out.println("Arquivo: " + file.getFileName() + " Upload");
+			InputStream inputstream = null;
+
+			try {
+				inputstream = file.getInputstream();
+			} catch (IOException e1) {
+				System.out.println("Erro getInputStream");
+			}
+			
+			try (Excel excel = new Excel(inputstream)) {
+				sheets = excel.getSheets();
+			} catch (Exception e1) {
+				System.out.println("Erro ao costruir Excel");
+			}
+		}
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+	
+	public List<Sheet> getSheets() {
+		return sheets;
+	}
 }
