c9a6738be99c987091eb620effef6aea827e8d82
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 20:03:22 -0300

    Removendo variavel InputStream e adicionando byte[], e implementando método run

--- src/main/java/br/net/altcom/excel/RepresentanteExcel.java
+++ src/main/java/br/net/altcom/excel/RepresentanteExcel.java
@@ -1,25 +1,35 @@
 package br.net.altcom.excel;
 
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
-public class RepresentanteExcel implements Serializable, Runnable{
+public class RepresentanteExcel implements Serializable, Runnable {
 
 	private static final long serialVersionUID = 1L;
-	private InputStream inputStream;
 	private String sheetName;
-	
+	private byte[] contents;
+
 	@Override
 	public void run() {
 		System.out.println("Executando RepresentanteExcel");
 		System.out.println("Planilha que sera executada: " + sheetName);
+
+		try (Excel excel = new Excel(new ByteArrayInputStream(contents))) {
+			System.out.println("Abriu inputStream");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
 	}
-	
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
-	
+
 	public void setSheetName(String sheetName) {
 		this.sheetName = sheetName;
+	}
+
+	public void setByte(byte[] contents) {
+		this.contents = contents;
 	}
 }
