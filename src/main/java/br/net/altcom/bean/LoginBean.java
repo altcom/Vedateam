664180a902379d610c9da171c27a8c92540841d5
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 12:14:07 -0300

    Implementando o método login da classe LoginBean

--- src/main/java/br/net/altcom/bean/LoginBean.java
+++ src/main/java/br/net/altcom/bean/LoginBean.java
@@ -10,14 +10,27 @@ import br.net.altcom.modelo.entity.Usuario;
 public class LoginBean {
 
 	private Usuario usuario = new Usuario();
-	
+
 	@Inject
 	private UsuarioDAO usuarioDAO;
-	
-	public String login(){
+
+	public String login() {
+		Usuario usuarioDoBanco = usuarioDAO.buscaPeloEmail(usuario);
+		
+		if (usuarioDoBanco == null){
+			System.out.println("Usuario não encontrado");			
+			return "";
+		}
+		
+		if (usuarioDoBanco.getSenha().equals(this.usuario.getSenha())){
+			System.out.println("Usuario Logado");
+			return "";
+		}
+		
+		System.out.println("Senha inválida");
 		return "";
 	}
-	
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
