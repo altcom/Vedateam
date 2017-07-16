f7556a692cfda33fafc8980113bf6e71221d226f
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 14:44:49 -0300

    Criando classe abstrata CRUD

---
+++ src/main/java/br/net/altcom/dao/CRUD.java
@@ -0,0 +1,26 @@
+package br.net.altcom.dao;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+public abstract class CRUD<T> {
+
+	@Inject
+	private EntityManager manager;
+
+	public void adiciona(T t) {
+		this.manager.persist(t);
+	}
+
+	public void altera(T t) {
+		this.manager.merge(t);
+	}
+
+	public void deleta(T t) {
+		this.manager.remove(t);
+	}
+
+	public void busca(T t) {
+		this.manager.find(t.getClass(), t);
+	}
+}
