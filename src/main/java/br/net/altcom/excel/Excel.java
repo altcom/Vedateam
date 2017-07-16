8bf317bdd4ce3d49c7ecb24eac9759cf954ca3e9
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 20:02:03 -0300

    Adicionando método getExcelSheetByName, e alterando método close

--- src/main/java/br/net/altcom/excel/Excel.java
+++ src/main/java/br/net/altcom/excel/Excel.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import com.monitorjbl.xlsx.StreamingReader;
@@ -17,24 +18,36 @@ public class Excel implements AutoCloseable {
 		this.inputStream = stream;
 	}
 
-	public List<String> getSheetsName(){
+	public List<String> getSheetsName() {
 		List<String> sheetsName = new ArrayList<>();
 		workbook = StreamingReader.builder().open(inputStream);
 		workbook.forEach(sheet -> sheetsName.add(sheet.getSheetName()));
-		
+
 		return sheetsName;
 	}
-	
+
+	public ExcelSheet getExcelSheetByName(String sheetName) {
+		workbook = StreamingReader.builder().open(inputStream);
+		for (Sheet sheet : workbook) {
+			if (sheet.getSheetName().equalsIgnoreCase(sheetName))
+				return new ExcelSheet(sheet);
+		}
+		return null;
+	}
+
 	public List<ExcelSheet> getExcelSheets() {
 		List<ExcelSheet> sheets = new ArrayList<>();
 		workbook = StreamingReader.builder().rowCacheSize(50000).bufferSize(6000).open(inputStream);
 		workbook.forEach(sheet -> sheets.add(new ExcelSheet(sheet)));
-		
+
 		return sheets;
 	}
 
 	@Override
 	public void close() throws Exception {
-		workbook.close();
+		if (workbook != null)
+			workbook.close();
+		if (inputStream != null)
+			inputStream.close();
 	}
 }
