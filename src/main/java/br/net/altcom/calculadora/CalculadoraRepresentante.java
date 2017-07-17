819faa3e7d532376944502230582b819803254cc
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 19:23:56 -0300

    Criando o método calculaPorcentagemDoHabilitador e utilizando na pagina representante.xhtml

--- src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
+++ src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
@@ -2,6 +2,7 @@ package br.net.altcom.calculadora;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Month;
 import java.time.YearMonth;
 
@@ -13,40 +14,51 @@ import br.net.altcom.dao.exception.MetaNaoEncontradaException;
 import br.net.altcom.modelo.entity.Meta;
 import br.net.altcom.modelo.entity.Representante;
 
-public class CalculadoraRepresentante implements Serializable{
+public class CalculadoraRepresentante implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Meta meta;
-	private BigDecimal progressoMeta = BigDecimal.ZERO; 
-	
+	private BigDecimal progressoMeta = BigDecimal.ZERO;
+	private BigDecimal porcentagemDoHabilitador = BigDecimal.ZERO;
+
 	@Inject
 	private MetaDAO metaDAO;
 	@Inject
 	private FaturamentoDAO faturamentoDAO;
 	private YearMonth data = YearMonth.of(2017, Month.APRIL);
-	
-	public void calcula(Representante representante){
+
+	public void calcula(Representante representante) {
 		try {
 			buscaMeta(representante);
 			buscaProgressoMeta(representante);
+			calculaPorcentagemDoHabilitador();
 		} catch (MetaNaoEncontradaException e) {
 			System.out.println("Meta não encontrada");
 		}
 	}
-	
-	private void buscaMeta(Representante representante) throws MetaNaoEncontradaException{
+
+	private void buscaMeta(Representante representante) throws MetaNaoEncontradaException {
 		this.meta = metaDAO.buscaMetaDoRepresentante(representante, "4-2017");
 	}
-	
-	private void buscaProgressoMeta(Representante representante){
+
+	private void buscaProgressoMeta(Representante representante) {
 		progressoMeta = faturamentoDAO.somaDoMesDeUmRepresentante(representante, data);
 	}
-	
+
+	private BigDecimal calculaPorcentagemDoHabilitador() {
+		BigDecimal metaFaturamento = this.meta.getFaturamento();
+		return porcentagemDoHabilitador = this.progressoMeta.divide(metaFaturamento, 2, RoundingMode.CEILING);
+	}
+
 	public BigDecimal getMeta() {
 		return meta.getFaturamento();
 	}
-	
+
 	public BigDecimal getProgressoMeta() {
 		return progressoMeta;
+	}
+
+	public BigDecimal getPorcentagemDoHabilitador() {
+		return porcentagemDoHabilitador;
 	}
 }
