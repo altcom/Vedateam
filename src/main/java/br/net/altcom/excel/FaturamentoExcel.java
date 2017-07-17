04de818d15b10db247cf8d074a4aecfc166f175e
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 16:49:27 -0300

    Implementando o método FaturamentoExcel

--- src/main/java/br/net/altcom/excel/FaturamentoExcel.java
+++ src/main/java/br/net/altcom/excel/FaturamentoExcel.java
@@ -3,16 +3,21 @@ package br.net.altcom.excel;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.apache.poi.ss.usermodel.Row;
 
+import br.net.altcom.dao.ClienteDAO;
+import br.net.altcom.dao.FaturamentoDAO;
 import br.net.altcom.dao.ProdutoDAO;
 import br.net.altcom.dao.RegionalDAO;
 import br.net.altcom.dao.RepresentanteDAO;
 import br.net.altcom.modelo.entity.Cliente;
+import br.net.altcom.modelo.entity.Faturamento;
 import br.net.altcom.modelo.entity.Produto;
 import br.net.altcom.modelo.entity.Regional;
 import br.net.altcom.modelo.entity.Representante;
@@ -27,6 +32,10 @@ public class FaturamentoExcel extends ExcelProcessor implements Serializable {
 	private RepresentanteDAO representanteDAO;
 	@Inject
 	private ProdutoDAO produtoDAO;
+	@Inject
+	private ClienteDAO clienteDAO;
+	@Inject
+	private FaturamentoDAO faturamentoDAO;
 
 	@Override
 	public void run() {
@@ -39,7 +48,7 @@ public class FaturamentoExcel extends ExcelProcessor implements Serializable {
 
 			while (!excelSheet.isFinish()) {
 				List<Row> rows = excelSheet.getPlusRow(10);
-
+				System.out.println(excelSheet.getLastPositionOfTheRow());
 				for (Row row : rows) {
 					getFaturamento(row);
 				}
@@ -53,10 +62,25 @@ public class FaturamentoExcel extends ExcelProcessor implements Serializable {
 	}
 
 	private void getFaturamento(Row row) {
-		Regional regional = new Regional();
-		regional.setNome(row.getCell(0).getStringCellValue());
-		regional = regionalDAO.buscaPeloNome(regional);
+		Regional regional = getRegional(row);
+		Representante representante = getRepresentante(row);
+		Produto produto = getProduto(row);
+		Cliente cliente = getCliente(row);
+		LocalDate data = getData(row);
+		BigDecimal valorFaturamento = new BigDecimal(row.getCell(12).getStringCellValue().replace(",", "."));
 
+		Faturamento faturamento = new Faturamento();
+		faturamento.setCliente(cliente);
+		faturamento.setProduto(produto);
+		faturamento.setRegional(regional);
+		faturamento.setRepresentante(representante);
+		faturamento.setData(data);
+		faturamento.setFaturamento(valorFaturamento);
+
+		faturamentoDAO.adiciona(faturamento);
+	}
+
+	private Representante getRepresentante(Row row) {
 		Representante representante = new Representante();
 		representante.setNome(row.getCell(2).getStringCellValue());
 
@@ -68,11 +92,35 @@ public class FaturamentoExcel extends ExcelProcessor implements Serializable {
 		representante.setCodigo(codigo);
 
 		representante = representanteDAO.buscaPeloCodigo(representante);
+		return representante;
+	}
 
-		Produto produto = getProduto(row);
-		
+	private Regional getRegional(Row row) {
+		Regional regional = new Regional();
+		regional.setNome(row.getCell(0).getStringCellValue());
+		regional = regionalDAO.buscaPeloNome(regional);
+		return regional;
+	}
+
+	public LocalDate getData(Row row) {
+		int dia = Integer.parseInt(row.getCell(10).getStringCellValue());
+		int mes = Integer.parseInt(row.getCell(9).getStringCellValue());
+		int ano = Integer.parseInt(row.getCell(8).getStringCellValue());
+		return LocalDate.of(ano, mes, dia);
+	}
+
+	private Cliente getCliente(Row row) {
 		Cliente cliente = new Cliente();
 		cliente.setNome(row.getCell(7).getStringCellValue());
+
+		Cliente clienteDoBanco = clienteDAO.buscaPeloNome(cliente);
+
+		if (clienteDoBanco == null) {
+			clienteDAO.adiciona(cliente);
+			return clienteDAO.buscaPeloNome(cliente);
+		} else {
+			return clienteDoBanco;
+		}
 	}
 
 	private Produto getProduto(Row row) {
