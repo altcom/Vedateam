d93ef1ddf16fed65ea43689a8d924d5441f4fb75
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 21:37:47 -0300

    Criando RegionalExcel

---
+++ src/main/java/br/net/altcom/excel/RegionalExcel.java
@@ -0,0 +1,13 @@
+package br.net.altcom.excel;
+
+import java.io.Serializable;
+
+public class RegionalExcel extends ExcelProcessor implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run() {
+		
+	}
+}
