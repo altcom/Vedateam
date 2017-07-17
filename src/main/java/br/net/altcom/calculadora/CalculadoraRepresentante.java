97e00c17258fa093f6e8a108a236d04368d12a51
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 19:01:04 -0300

    Criando os métodos calcula, buscaMeta e um atributo do tipo Meta na classe CalculadoraRepresentante

--- src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
+++ src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
@@ -1,11 +1,37 @@
 package br.net.altcom.calculadora;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.inject.Inject;
+
+import br.net.altcom.dao.MetaDAO;
+import br.net.altcom.dao.exception.MetaNaoEncontradaException;
+import br.net.altcom.modelo.entity.Meta;
+import br.net.altcom.modelo.entity.Representante;
 
 public class CalculadoraRepresentante implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Inject
+	private MetaDAO metaDAO;
 
+	private Meta meta;
+	
+	public void calcula(Representante representante){
+		try {
+			buscaMeta(representante);
+		} catch (MetaNaoEncontradaException e) {
+			System.out.println("Meta não encontrada");
+		}
+	}
+	
+	private void buscaMeta(Representante representante) throws MetaNaoEncontradaException{
+		this.meta = metaDAO.buscaMetaDoRepresentante(representante, "4-2017");
+	}
+	
+	public BigDecimal getMeta() {
+		return meta.getFaturamento();
+	}
 }
