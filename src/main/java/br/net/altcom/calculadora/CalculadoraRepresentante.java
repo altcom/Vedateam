e20ad1e6ebf521b0cd32495ab1ea7d101741e04c
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 18:42:10 -0300

    Criando calculadoraRepresentante

---
+++ src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
@@ -0,0 +1,11 @@
+package br.net.altcom.calculadora;
+
+import java.io.Serializable;
+
+public class CalculadoraRepresentante implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
