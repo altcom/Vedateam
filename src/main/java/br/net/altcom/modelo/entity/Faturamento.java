fa1f6e501b9f7448ffd723731488ebaf995e1bf2
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 13:53:34 -0300

    Adicionando getters e setters

--- src/main/java/br/net/altcom/modelo/entity/Faturamento.java
+++ src/main/java/br/net/altcom/modelo/entity/Faturamento.java
@@ -8,9 +8,10 @@ import javax.persistence.OneToOne;
 @Entity
 public class Faturamento {
 
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
 	private Integer id;
-	
+
 	@OneToOne
 	private Regional regional;
 	@OneToOne
@@ -19,4 +20,36 @@ public class Faturamento {
 	private Produto produto;
 	@OneToOne
 	private Cliente cliente;
+
+	public Regional getRegional() {
+		return regional;
+	}
+
+	public void setRegional(Regional regional) {
+		this.regional = regional;
+	}
+
+	public Representante getRepresentante() {
+		return representante;
+	}
+
+	public void setRepresentante(Representante representante) {
+		this.representante = representante;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 }
