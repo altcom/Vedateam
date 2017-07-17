f8488d7ef95b22c33eb387e36d736ba0e9a6bbb4
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 13:10:57 -0300

    Criando método buscaPeloCodigo na classe ProdutoDAO

--- src/main/java/br/net/altcom/dao/ProdutoDAO.java
+++ src/main/java/br/net/altcom/dao/ProdutoDAO.java
@@ -1,10 +1,22 @@
 package br.net.altcom.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
 import br.net.altcom.modelo.entity.Produto;
 
 @Stateless
-public class ProdutoDAO extends CRUD<Produto>{
+public class ProdutoDAO extends CRUD<Produto> {
 
+	public Produto buscaPeloCodigo(Produto produto) {
+		String jpql = "SELECT p FROM Produto p WHERE p.codigo = :codigo";
+		TypedQuery<Produto> query = manager.createQuery(jpql, Produto.class);
+		query.setParameter("codigo", produto.getCodigo());
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
 }
