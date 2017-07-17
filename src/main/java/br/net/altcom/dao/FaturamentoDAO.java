a4e04056f5a0c03e275e0e230b33af3ae54397f3
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 19:48:21 -0300

    Adicionando o método buscarClientesAtivosDoMesDeUmRepresentante na classe FaturamentoDAO

--- src/main/java/br/net/altcom/dao/FaturamentoDAO.java
+++ src/main/java/br/net/altcom/dao/FaturamentoDAO.java
@@ -2,10 +2,13 @@ package br.net.altcom.dao;
 
 import java.math.BigDecimal;
 import java.time.YearMonth;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.ejb.Stateless;
 import javax.persistence.TypedQuery;
 
+import br.net.altcom.modelo.entity.Cliente;
 import br.net.altcom.modelo.entity.Faturamento;
 import br.net.altcom.modelo.entity.Representante;
 
@@ -22,5 +25,19 @@ public class FaturamentoDAO extends CRUD<Faturamento> {
 		
 		BigDecimal total = query.getSingleResult();
 		return (total == null) ? BigDecimal.ZERO : total;
+	}
+	
+	public Set<Cliente> buscarClientesAtivosDoMesDeUmRepresentante(Representante representante, YearMonth mes){
+		String jpql = "SELECT f.cliente FROM Faturamento f "
+				+ "WHERE f.representante = :representante AND MONTH(f.data) = :mes AND YEAR(f.data) = :ano";
+		TypedQuery<Cliente> query = manager.createQuery(jpql, Cliente.class);
+		query.setParameter("representante", representante);
+		query.setParameter("mes", mes.getMonthValue());
+		query.setParameter("ano", mes.getYear());
+		
+		Set<Cliente> clientesAtivos = new HashSet<>();
+		query.getResultList().forEach(c -> clientesAtivos.add(c));
+		
+		return clientesAtivos;
 	}
 }
