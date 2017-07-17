4a4e4a27007d23583db8822b24ac012b4f17974c
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 12:53:48 -0300

    Adicionando método buscaPeloNome

--- src/main/java/br/net/altcom/dao/RegionalDAO.java
+++ src/main/java/br/net/altcom/dao/RegionalDAO.java
@@ -1,10 +1,22 @@
 package br.net.altcom.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
 import br.net.altcom.modelo.entity.Regional;
 
 @Stateless
-public class RegionalDAO extends CRUD<Regional>{
+public class RegionalDAO extends CRUD<Regional> {
 
+	public Regional buscaPeloNome(Regional regional) {
+		String jpql = "SELECT r FROM Regional r WHERE r.nome LIKE :nome";
+		TypedQuery<Regional> query = manager.createQuery(jpql, Regional.class);
+		query.setParameter("nome", regional.getNome());
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
 }
