02a0c92731015881f879ea62e5130fba7ea80308
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-16 21:13:28 -0300

    Criando entidade Regional

---
+++ src/main/java/br/net/altcom/modelo/entity/Regional.java
@@ -0,0 +1,41 @@
+package br.net.altcom.modelo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Regional {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer codigo;
+	private String nome;
+	private String email;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
