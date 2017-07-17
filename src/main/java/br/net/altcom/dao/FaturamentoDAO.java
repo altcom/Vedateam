5c9441da66480462d7844de2e02c82da31484d40
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 20:09:22 -0300

    Criando o método buscaClientesEntreMes na classe FaturamentoDAO

--- src/main/java/br/net/altcom/dao/FaturamentoDAO.java
+++ src/main/java/br/net/altcom/dao/FaturamentoDAO.java
@@ -1,6 +1,7 @@
 package br.net.altcom.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,22 +23,41 @@ public class FaturamentoDAO extends CRUD<Faturamento> {
 		query.setParameter("representante", representante);
 		query.setParameter("mes", mes.getMonthValue());
 		query.setParameter("ano", mes.getYear());
-		
+
 		BigDecimal total = query.getSingleResult();
 		return (total == null) ? BigDecimal.ZERO : total;
 	}
-	
-	public Set<Cliente> buscarClientesAtivosDoMesDeUmRepresentante(Representante representante, YearMonth mes){
+
+	public Set<Cliente> buscarClientesAtivosDoMesDeUmRepresentante(Representante representante, YearMonth mes) {
 		String jpql = "SELECT f.cliente FROM Faturamento f "
 				+ "WHERE f.representante = :representante AND MONTH(f.data) = :mes AND YEAR(f.data) = :ano";
 		TypedQuery<Cliente> query = manager.createQuery(jpql, Cliente.class);
 		query.setParameter("representante", representante);
 		query.setParameter("mes", mes.getMonthValue());
 		query.setParameter("ano", mes.getYear());
-		
+
 		Set<Cliente> clientesAtivos = new HashSet<>();
 		query.getResultList().forEach(c -> clientesAtivos.add(c));
-		
+
 		return clientesAtivos;
+	}
+
+	public Set<Cliente> buscaClientesEntreMes(Representante representante, YearMonth mesInicial, YearMonth mesFinal) {
+
+		LocalDate dataInicial = LocalDate.of(mesInicial.getYear(), mesInicial.getMonth(), 1);
+		LocalDate dataFinal = LocalDate.of(mesFinal.getYear(), mesFinal.getMonth(), mesFinal.lengthOfMonth());
+
+		String jpql = "SELECT f.cliente FROM Faturamento f WHERE f.representante = :representante "
+				+ "AND f.data BETWEEN :dataInicial AND :dataFinal";
+
+		TypedQuery<Cliente> query = manager.createQuery(jpql, Cliente.class);
+		query.setParameter("representante", representante);
+		query.setParameter("dataInicial", dataInicial);
+		query.setParameter("dataFinal", dataFinal);
+		
+		Set<Cliente> clientesNaBase = new HashSet<>();
+		query.getResultList().forEach(c -> clientesNaBase.add(c));
+		
+		return clientesNaBase;
 	}
 }
