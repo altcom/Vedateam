2fb811a425cfc5497af9dacfd8cdd8ca04cdeee4
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 18:51:25 -0300

    Adicionando o método buscaMetaDoRepresentante na classe MetaDAO

--- src/main/java/br/net/altcom/dao/MetaDAO.java
+++ src/main/java/br/net/altcom/dao/MetaDAO.java
@@ -1,10 +1,26 @@
 package br.net.altcom.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
+import br.net.altcom.dao.exception.MetaNaoEncontradaException;
 import br.net.altcom.modelo.entity.Meta;
+import br.net.altcom.modelo.entity.Representante;
 
 @Stateless
 public class MetaDAO extends CRUD<Meta> {
+
+	public Meta buscaMetaDoRepresentante(Representante representante, String mes) throws MetaNaoEncontradaException {
+		String jpql = "SELECT m FROM Meta m WHERE m.codigo = :codigo AND m.mes LIKE :mes";
+		TypedQuery<Meta> query = manager.createQuery(jpql, Meta.class);
+		query.setParameter("codigo", representante.getCodigo());
+		query.setParameter("mes", mes);
+		try{			
+			return query.getSingleResult();
+		}catch(NoResultException e){
+			throw new MetaNaoEncontradaException();
+		}
+	}
 
 }
---
+++ src/main/java/br/net/altcom/dao/exception/MetaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.net.altcom.dao.exception;
+
+public class MetaNaoEncontradaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
