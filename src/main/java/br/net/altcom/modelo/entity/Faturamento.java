215e121740bfcc9c3dad54cd9f657abd2de5c1d8
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 16:49:04 -0300

    Adicionando atributo data e faturamento na classe Faturamento

--- src/main/java/br/net/altcom/modelo/entity/Faturamento.java
+++ src/main/java/br/net/altcom/modelo/entity/Faturamento.java
@@ -1,5 +1,8 @@
 package br.net.altcom.modelo.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -21,6 +24,9 @@ public class Faturamento {
 	@OneToOne
 	private Cliente cliente;
 
+	private LocalDate data;
+	private BigDecimal faturamento;
+	
 	public Regional getRegional() {
 		return regional;
 	}
@@ -51,5 +57,21 @@ public class Faturamento {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public BigDecimal getFaturamento() {
+		return faturamento;
+	}
+
+	public void setFaturamento(BigDecimal faturamento) {
+		this.faturamento = faturamento;
 	}
 }
