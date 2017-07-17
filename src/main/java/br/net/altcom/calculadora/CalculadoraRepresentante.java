d82c7fdd7330448cfb3510a7514d5ccc1c0ef33e
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 19:16:09 -0300

    Criando o método buscaProgressoMeta e o atributo progressoMeta com getters

--- src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
+++ src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
@@ -2,9 +2,12 @@ package br.net.altcom.calculadora;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.Month;
+import java.time.YearMonth;
 
 import javax.inject.Inject;
 
+import br.net.altcom.dao.FaturamentoDAO;
 import br.net.altcom.dao.MetaDAO;
 import br.net.altcom.dao.exception.MetaNaoEncontradaException;
 import br.net.altcom.modelo.entity.Meta;
@@ -13,15 +16,19 @@ import br.net.altcom.modelo.entity.Representante;
 public class CalculadoraRepresentante implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	private Meta meta;
+	private BigDecimal progressoMeta = BigDecimal.ZERO; 
 	
 	@Inject
 	private MetaDAO metaDAO;
-
-	private Meta meta;
+	@Inject
+	private FaturamentoDAO faturamentoDAO;
+	private YearMonth data = YearMonth.of(2017, Month.APRIL);
 	
 	public void calcula(Representante representante){
 		try {
 			buscaMeta(representante);
+			buscaProgressoMeta(representante);
 		} catch (MetaNaoEncontradaException e) {
 			System.out.println("Meta não encontrada");
 		}
@@ -31,7 +38,15 @@ public class CalculadoraRepresentante implements Serializable{
 		this.meta = metaDAO.buscaMetaDoRepresentante(representante, "4-2017");
 	}
 	
+	private void buscaProgressoMeta(Representante representante){
+		progressoMeta = faturamentoDAO.somaDoMesDeUmRepresentante(representante, data);
+	}
+	
 	public BigDecimal getMeta() {
 		return meta.getFaturamento();
+	}
+	
+	public BigDecimal getProgressoMeta() {
+		return progressoMeta;
 	}
 }
