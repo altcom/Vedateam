2e7526db27cd49fe11b078981d9bb1f4c5397509
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-17 13:57:20 -0300

    Criando o faturamentoDAO

---
+++ src/main/java/br/net/altcom/dao/FaturamentoDAO.java
@@ -0,0 +1,10 @@
+package br.net.altcom.dao;
+
+import javax.ejb.Stateless;
+
+import br.net.altcom.modelo.entity.Faturamento;
+
+@Stateless
+public class FaturamentoDAO extends CRUD<Faturamento>{
+
+}
