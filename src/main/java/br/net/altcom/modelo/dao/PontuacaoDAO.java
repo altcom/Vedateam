3466a0ba8928109e34a8ae1266065b180a1a6e64
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-19 17:29:59 -0300

    Criando método buscaPorMesDoRepresentante da classe PontuacaoDAO

--- src/main/java/br/net/altcom/modelo/dao/PontuacaoDAO.java
+++ src/main/java/br/net/altcom/modelo/dao/PontuacaoDAO.java
@@ -1,8 +1,28 @@
 package br.net.altcom.modelo.dao;
 
+import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
 import br.net.altcom.dao.CRUD;
 import br.net.altcom.modelo.entity.Pontuacao;
+import br.net.altcom.modelo.entity.Representante;
 
+@Stateless
 public class PontuacaoDAO extends CRUD<Pontuacao> {
 
+	public Pontuacao buscaPorMesDoRepresentante(Representante representante, String mes) {
+		String jpql = "SELECT p FROM Pontuacao p WHERE p.codigo = :codigo AND p.mes LIKE :mes";
+		TypedQuery<Pontuacao> query = manager.createQuery(jpql, Pontuacao.class);
+		query.setParameter("codigo", representante.getCodigo());
+		query.setParameter("mes", mes);
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException e) {
+			Pontuacao pontuacao = new Pontuacao();
+			pontuacao.setCodigo(representante.getCodigo());
+			pontuacao.setMes(mes);
+			return pontuacao;
+		}
+	}
 }
