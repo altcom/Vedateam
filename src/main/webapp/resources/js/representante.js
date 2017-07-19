554b27bbcfa2f9b168838a0464e497af1f831c20
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-19 17:38:19 -0300

    Alterando representante.xhtml e representante.js

--- src/main/webapp/resources/js/representante.js
+++ src/main/webapp/resources/js/representante.js
@@ -10,6 +10,55 @@ $(document).ready(function(){
         	value: '<p>' + parseInt(faturamentoPorcentagem * 100) + '%</p>'
         }
     });
+	circleFaturamento.animate(Math.min(faturamentoPorcentagem, 1), 1);
 	
-	circleFaturamento.animate(faturamentoPorcentagem, 1);
+	var btnClienteNovos = $("#btn-clientesNovos");
+	var btnClientesAtivos = $("#btn-clientesAtivos");
+	var btnClientesNaBase = $("#btn-clientesNaBase");
+	
+	var btnCN = false;
+	var btnCA = false;
+	var btnCB = false;
+	
+	btnClienteNovos.click(function() {
+		var icone = $("#btn-clientesNovos .glyphicon"); 
+		if(!btnCN){
+			icone.removeClass("glyphicon-triangle-bottom");
+			icone.addClass("glyphicon-triangle-top");
+			$("#clientesNovos").css("display", "block");
+		}else{
+			icone.removeClass("glyphicon-triangle-top");
+			icone.addClass("glyphicon-triangle-bottom");
+			$("#clientesNovos").css("display", "none");
+		}
+		btnCN = !btnCN;
+	});
+	
+	btnClientesAtivos.click(function() {
+		var icone = $("#btn-clientesAtivos .glyphicon"); 
+		if(!btnCA){
+			icone.removeClass("glyphicon-triangle-bottom");
+			icone.addClass("glyphicon-triangle-top");
+			$("#clientesAtivos").css("display", "block");
+		}else{
+			icone.removeClass("glyphicon-triangle-top");
+			icone.addClass("glyphicon-triangle-bottom");
+			$("#clientesAtivos").css("display", "none");
+		}
+		btnCA = !btnCA;
+	});
+	
+	btnClientesNaBase.click(function() {
+		var icone = $("#btn-clientesNaBase .glyphicon"); 
+		if(!btnCB){
+			icone.removeClass("glyphicon-triangle-bottom");
+			icone.addClass("glyphicon-triangle-top");
+			$("#clientesNaBase").css("display", "block");
+		}else{
+			icone.removeClass("glyphicon-triangle-top");
+			icone.addClass("glyphicon-triangle-bottom");
+			$("#clientesNaBase").css("display", "none");
+		}
+		btnCB = !btnCB;
+	});
 });
