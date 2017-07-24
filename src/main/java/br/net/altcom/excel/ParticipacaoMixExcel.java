b7f7a0e2753c930d45fde7d28e1818de6266b4c9
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-24 15:54:20 -0300

    Criando ParticipacaoMixExcel

---
+++ src/main/java/br/net/altcom/excel/ParticipacaoMixExcel.java
@@ -0,0 +1,70 @@
+package br.net.altcom.excel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import br.net.altcom.dao.RepresentanteDAO;
+import br.net.altcom.modelo.dao.ParticipacaoMixDAO;
+import br.net.altcom.modelo.entity.ParticipacaoMix;
+import br.net.altcom.modelo.entity.Representante;
+
+public class ParticipacaoMixExcel extends ExcelProcessor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private RepresentanteDAO representanteDAO;
+	@Inject
+	private ParticipacaoMixDAO participacaoDAO; 
+	private Row header = null;
+
+	@Override
+	protected void executa(ExcelSheet excelSheet) {
+		header = excelSheet.getHeader();
+		List<Row> rows = excelSheet.getPlusRow(10);
+
+		for (Row row : rows) {
+			getParticipacaoMix(row);
+		}
+	}
+
+	private void getParticipacaoMix(Row row){
+		Representante representante = null;
+		String codigo = "";
+		for (Cell cell : header) {
+			if (cell.getColumnIndex() == 0) {
+				codigo = row.getCell(cell.getColumnIndex()).getStringCellValue();
+				representante = getRepresentante(codigo);
+				continue;
+			}
+			
+			if(representante == null){
+				System.out.println("Não foi encontrado representante: " + codigo);
+				return;
+			}
+			
+			ParticipacaoMix participacaoMix = new ParticipacaoMix();
+			participacaoMix.setRepresentante(representante);
+			participacaoMix.setFamilia(cell.getStringCellValue());
+			
+			String porcentagem = row.getCell(cell.getColumnIndex()).getStringCellValue();
+			porcentagem = porcentagem.replaceAll(",", "").replace("%", "");
+			participacaoMix.setPorcentagem(new BigDecimal(porcentagem));
+			participacaoMix.setMes("4-2017");
+			
+			participacaoDAO.adiciona(participacaoMix);
+		}	
+	}
+	
+	private Representante getRepresentante(String codigo) {
+		Representante representante = new Representante();
+		representante.setCodigo(new Integer(codigo));
+		return representanteDAO.buscaPeloCodigo(representante);
+	}
+}
