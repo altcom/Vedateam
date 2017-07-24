08a2269f0b6b87f7691ff72ee0488cfefb15d00b
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-24 02:39:31 -0300

    Criando a classe participacaoMixDAO

---
+++ src/main/java/br/net/altcom/modelo/dao/ParticipacaoMixDAO.java
@@ -0,0 +1,11 @@
+package br.net.altcom.modelo.dao;
+
+import javax.ejb.Stateless;
+
+import br.net.altcom.dao.CRUD;
+import br.net.altcom.modelo.entity.ParticipacaoMix;
+
+@Stateless
+public class ParticipacaoMixDAO extends CRUD<ParticipacaoMix>{
+
+}
