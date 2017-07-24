4f39f9a9e9794b493b1077405fa596861f3247f3
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-24 16:29:30 -0300

    Criando o método buscaDoMesDeUmRepresentante

--- src/main/java/br/net/altcom/modelo/dao/ParticipacaoMixDAO.java
+++ src/main/java/br/net/altcom/modelo/dao/ParticipacaoMixDAO.java
@@ -1,11 +1,22 @@
 package br.net.altcom.modelo.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 import br.net.altcom.dao.CRUD;
 import br.net.altcom.modelo.entity.ParticipacaoMix;
+import br.net.altcom.modelo.entity.Representante;
 
 @Stateless
 public class ParticipacaoMixDAO extends CRUD<ParticipacaoMix>{
+
+	public List<ParticipacaoMix> buscaDoMesDeUmRepresentante(Representante representante) {
+		String jpql = "SELECT p FROM ParticipacaoMix p WHERE p.representante = :representante";
+		TypedQuery<ParticipacaoMix> query = manager.createQuery(jpql, ParticipacaoMix.class);
+		query.setParameter("representante", representante);
+		return query.getResultList();
+	}
 
 }
