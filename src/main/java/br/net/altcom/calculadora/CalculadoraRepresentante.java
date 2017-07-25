772d581e82c9e00b0459b38c231808aebbc98be6
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-25 00:40:54 -0300

    Criando método buscaTotalDeCadaFamilia e buscaParticipacaoMix na classe CalculadoraRepresentante

--- src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
+++ src/main/java/br/net/altcom/calculadora/CalculadoraRepresentante.java
@@ -6,6 +6,8 @@ import java.math.RoundingMode;
 import java.time.Month;
 import java.time.YearMonth;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -13,8 +15,11 @@ import javax.inject.Inject;
 import br.net.altcom.dao.FaturamentoDAO;
 import br.net.altcom.dao.MetaDAO;
 import br.net.altcom.dao.exception.MetaNaoEncontradaException;
+import br.net.altcom.modelo.dao.Mix;
+import br.net.altcom.modelo.dao.ParticipacaoMixDAO;
 import br.net.altcom.modelo.entity.Cliente;
 import br.net.altcom.modelo.entity.Meta;
+import br.net.altcom.modelo.entity.ParticipacaoMix;
 import br.net.altcom.modelo.entity.Representante;
 
 public class CalculadoraRepresentante implements Serializable {
@@ -25,24 +30,28 @@ public class CalculadoraRepresentante implements Serializable {
 	private BigDecimal progressoMeta = BigDecimal.ZERO;
 	private BigDecimal porcentagemDoHabilitador = BigDecimal.ZERO;
 	private int pontosHabilitador;
+	private int pontosClientesNovo;
 
 	private Set<Cliente> clientesAtivos = new HashSet<>();
 	private Set<Cliente> clientesNaBase = new HashSet<>();
 	private Set<Cliente> clientesNovo = new HashSet<>();
+	private List<ParticipacaoMix> participacoes;
 
 	@Inject
 	private MetaDAO metaDAO;
 	@Inject
 	private FaturamentoDAO faturamentoDAO;
+	@Inject
+	private ParticipacaoMixDAO participacaoDAO;
 
 	private YearMonth data = YearMonth.of(2017, Month.APRIL);
-	private int pontosClientesNovo;
 
 	public void calcula(Representante representante) {
 		try {
 			buscaMeta(representante);
 			buscaProgressoMeta(representante);
-			
+			buscaParticipacaoMix(representante, data);
+
 			calcularPontosDoHabilitador();
 			calcularPontosClienteNovos(representante);
 		} catch (MetaNaoEncontradaException e) {
@@ -50,21 +59,21 @@ public class CalculadoraRepresentante implements Serializable {
 		}
 	}
 
-	private void calcularPontosDoHabilitador(){
+	private void calcularPontosDoHabilitador() {
 		calculaPorcentagemDoHabilitador();
 		BigDecimal porcentagem = this.porcentagemDoHabilitador.multiply(new BigDecimal("100"));
 		pontosHabilitador = TabelaDePontos.pontosHabilitadorRepresentante(porcentagem.doubleValue());
 	}
-	
-	private void calcularPontosClienteNovos(Representante representante){
+
+	private void calcularPontosClienteNovos(Representante representante) {
 		pegaClientesNovos(representante);
 		pontosClientesNovo = TabelaDePontos.pontosClientesNovo(this.clientesNovo.size());
 	}
-	
-	public int getTotalDePontosAcumulados(){
-		return this.pontosHabilitador + this.pontosClientesNovo; 
+
+	public int getTotalDePontosAcumulados() {
+		return this.pontosHabilitador + this.pontosClientesNovo;
 	}
-	
+
 	private void buscaMeta(Representante representante) throws MetaNaoEncontradaException {
 		this.meta = metaDAO.buscaMetaDoRepresentante(representante, "4-2017");
 	}
@@ -79,6 +88,28 @@ public class CalculadoraRepresentante implements Serializable {
 
 	private void buscaClientesNaBase(Representante representante) {
 		clientesNaBase = faturamentoDAO.buscaClientesEntreMes(representante, data.minusMonths(6), data.minusMonths(1));
+	}
+
+	private Map<String, Mix> buscaTotalDeCadaFamilia(Representante representante, YearMonth mes) {
+		return faturamentoDAO.somaDoMesDeCadaFamiliaDeUmRepresentante(representante, mes);
+	}
+
+	private void buscaParticipacaoMix(Representante representante, YearMonth mes) {
+		Map<String, Mix> familias = buscaTotalDeCadaFamilia(representante, mes);
+		participacoes = participacaoDAO.buscaDoMesDeUmRepresentante(representante);
+
+		for (ParticipacaoMix participacao : participacoes) {
+			Mix mix = familias.get(participacao.getFamilia().toLowerCase());
+			
+			if (mix == null) {
+				participacao.setProgresso(new BigDecimal("0"));
+			} else {
+				participacao.setProgresso(mix.getProgresso());
+			}
+			BigDecimal porcentagem = participacao.getPorcentagem().divide(new BigDecimal("100"));
+			BigDecimal metaMix = meta.getFaturamento().multiply(porcentagem).setScale(2, RoundingMode.CEILING);
+			participacao.setMetaMix(metaMix);
+		}
 	}
 
 	private void pegaClientesNovos(Representante representante) {
@@ -104,11 +135,11 @@ public class CalculadoraRepresentante implements Serializable {
 	public BigDecimal getPorcentagemDoHabilitador() {
 		return porcentagemDoHabilitador;
 	}
-	
+
 	public int getPontosHabilitador() {
 		return pontosHabilitador;
 	}
-	
+
 	public int getPontosClientesNovo() {
 		return pontosClientesNovo;
 	}
@@ -123,5 +154,9 @@ public class CalculadoraRepresentante implements Serializable {
 
 	public Set<Cliente> getClientesNovo() {
 		return clientesNovo;
+	}
+	
+	public List<ParticipacaoMix> getParticipacoes() {
+		return participacoes;
 	}
 }
