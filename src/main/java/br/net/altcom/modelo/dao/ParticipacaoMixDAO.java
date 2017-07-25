2a1c42307e22919b938e4ba5ae19f6bcb9a48224
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-25 00:32:22 -0300

    Criando método somaDoMesDeCadaFamiliaDeUmRepresentante na classe ParticipacaoMixDAO

--- src/main/java/br/net/altcom/modelo/dao/ParticipacaoMixDAO.java
+++ src/main/java/br/net/altcom/modelo/dao/ParticipacaoMixDAO.java
@@ -10,12 +10,13 @@ import br.net.altcom.modelo.entity.ParticipacaoMix;
 import br.net.altcom.modelo.entity.Representante;
 
 @Stateless
-public class ParticipacaoMixDAO extends CRUD<ParticipacaoMix>{
+public class ParticipacaoMixDAO extends CRUD<ParticipacaoMix> {
 
 	public List<ParticipacaoMix> buscaDoMesDeUmRepresentante(Representante representante) {
 		String jpql = "SELECT p FROM ParticipacaoMix p WHERE p.representante = :representante";
 		TypedQuery<ParticipacaoMix> query = manager.createQuery(jpql, ParticipacaoMix.class);
 		query.setParameter("representante", representante);
+
 		return query.getResultList();
 	}
 
