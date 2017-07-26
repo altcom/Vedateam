6b20f6c372e989279a55885285ee02192aaea4b6
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-26 11:47:49 -0300

    Refatorando a classe MetaDAO juntando os métodos buscaMetaDoRepresentante e buscaMetaDoRegional para buscaMetaDoMes

--- src/main/java/br/net/altcom/dao/MetaDAO.java
+++ src/main/java/br/net/altcom/dao/MetaDAO.java
@@ -8,16 +8,14 @@ import javax.persistence.TypedQuery;
 
 import br.net.altcom.dao.exception.MetaNaoEncontradaException;
 import br.net.altcom.modelo.entity.Meta;
-import br.net.altcom.modelo.entity.Regional;
-import br.net.altcom.modelo.entity.Representante;
 
 @Stateless
 public class MetaDAO extends CRUD<Meta> {
 
-	public Meta buscaMetaDoRepresentante(Representante representante, YearMonth mes) throws MetaNaoEncontradaException {
+	public Meta buscaMetaDoMes(Integer codigo, YearMonth mes) throws MetaNaoEncontradaException {
 		String jpql = "SELECT m FROM Meta m WHERE m.codigo = :codigo AND m.mes LIKE :mes";
 		TypedQuery<Meta> query = manager.createQuery(jpql, Meta.class);
-		query.setParameter("codigo", representante.getCodigo());
+		query.setParameter("codigo", codigo);
 		query.setParameter("mes", mes.getMonthValue() + "/" + mes.getYear());
 		try {
 			return query.getSingleResult();
@@ -25,17 +23,4 @@ public class MetaDAO extends CRUD<Meta> {
 			throw new MetaNaoEncontradaException();
 		}
 	}
-
-	public Meta buscaMetaDoRegional(Regional regional, String mes) throws MetaNaoEncontradaException {
-		String jpql = "SELECT m FROM Meta m WHERE m.codigo = :codigo AND m.mes LIKE :mes";
-		TypedQuery<Meta> query = manager.createQuery(jpql, Meta.class);
-		query.setParameter("codigo", regional.getCodigo());
-		query.setParameter("mes", mes);
-		try {
-			return query.getSingleResult();
-		} catch (NoResultException e) {
-			throw new MetaNaoEncontradaException();
-		}
-	}
-
 }
