6cff5f7467e35441b49082f7455953039e0410c7
Francisco Lucas <franciscocostaalves2@gmail.com>
2017-07-25 23:06:26 -0300

    Alterando parametro do método buscaMetaDoRepresentante da classe MetaDAO

--- src/main/java/br/net/altcom/dao/MetaDAO.java
+++ src/main/java/br/net/altcom/dao/MetaDAO.java
@@ -1,5 +1,7 @@
 package br.net.altcom.dao;
 
+import java.time.YearMonth;
+
 import javax.ejb.Stateless;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
@@ -12,11 +14,11 @@ import br.net.altcom.modelo.entity.Representante;
 @Stateless
 public class MetaDAO extends CRUD<Meta> {
 
-	public Meta buscaMetaDoRepresentante(Representante representante, String mes) throws MetaNaoEncontradaException {
+	public Meta buscaMetaDoRepresentante(Representante representante, YearMonth mes) throws MetaNaoEncontradaException {
 		String jpql = "SELECT m FROM Meta m WHERE m.codigo = :codigo AND m.mes LIKE :mes";
 		TypedQuery<Meta> query = manager.createQuery(jpql, Meta.class);
 		query.setParameter("codigo", representante.getCodigo());
-		query.setParameter("mes", mes);
+		query.setParameter("mes", mes.getMonthValue() + "/" + mes.getYear());
 		try {
 			return query.getSingleResult();
 		} catch (NoResultException e) {
